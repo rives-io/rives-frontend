03b2cbcead9f083b17423c07de2b277f85807629
Arthur Vianna <arthurvianna@id.uff.br>
2024-06-27 18:11:29 -0300

    feat: tape page

--- app/tapes/[tape_id]/page.tsx
+++ app/tapes/[tape_id]/page.tsx
@@ -1,6 +1,12 @@
+import { CartridgeInfo, RuleInfo } from '@/app/backend-libs/core/ifaces';
+import { cartridgeInfo, getOutputs, rules, VerifyPayload } from '@/app/backend-libs/core/lib';
+import ContestCard from '@/app/components/ContestCard';
 import RivemuPlayer from '@/app/components/RivemuPlayer';
 import TapeAssetManager from '@/app/components/TapeAssetManager';
 import { envClient } from '@/app/utils/clientEnv';
+import { User, getUsersByAddress } from '@/app/utils/privyApi';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
 
 export async function generateMetadata({ params }: { params: { tape_id: string } }) {
@@ -28,12 +34,94 @@ export async function generateMetadata({ params }: { params: { tape_id: string }
 }
 
 export default async function Tape({ params }: { params: { tape_id: string } }) {
+    let res = (await getOutputs(
+        {
+            tags: ["tape", params.tape_id],
+        },
+        {cartesiNodeUrl: envClient.CARTESI_NODE_URL}
+    ));
+
+    if (res.data.length == 0) return notFound();
+    
+    const tape:VerifyPayload = res.data[0];
+
+    const userMap:Record<string,User> = JSON.parse(await getUsersByAddress([tape._msgSender]));
+    const user = userMap[tape._msgSender.toLowerCase()];
+    res = await rules({id: tape.rule_id.substring(2)}, {cartesiNodeUrl: envClient.CARTESI_NODE_URL, decode: true});
+    const contest:RuleInfo = res.data[0];
+
+    const tapeCartridge:CartridgeInfo = await cartridgeInfo({id: contest.cartridge_id}, {cartesiNodeUrl: envClient.CARTESI_NODE_URL, decode: true});
+
+
     return (
-        // <main className="flex items-center justify-center">
-        <main className="flex grid grid-cols-1 justify-items-center justify-center my-20">
+        <main className="grid grid-cols-1 justify-items-center justify-center w-full gap-4 px-4 md:px-0 ">
             <RivemuPlayer tape_id={params.tape_id}/>
             <div className='flex flex-wrap gap-4'>
                 <TapeAssetManager tape_id={params.tape_id}/>
+            </div>
+
+            <div className='w-full md:w-2/3 flex flex-col gap-2'>
+                <div className='flex flex-wrap gap-4'>
+                    <div className='flex flex-col w-full'>
+                        <h1 className={`pixelated-font text-2xl md:text-5xl truncate`}>{params.tape_id.substring(0, 20)}</h1>
+                        <span className='text-sm md:text-base truncate'>
+                        {
+                            !user?
+                                tape._msgSender
+                            :
+                                <Link href={`profile/${tape._msgSender}`} className='flex items-center gap-2'>
+                                    <img width={48} height={48} src={user? user.picture_url:""} className='rounded-full' alt='Nop' />
+                                    <span title={tape._msgSender}>{user.name}</span>
+                                </Link>
+                        }
+                        </span>
+                        <span>Cartrige: {tapeCartridge.name}</span>
+                    </div>
+
+                    <div className='justify-center md:justify-end flex-1 self-center text-black flex gap-2'>
+                        <button className='bg-[#e04ec3] p-2 text-center font-bold w-32 h-10 hover:scale-105'>
+                            ${0.09} Sell
+                        </button>
+
+                        <button className='bg-[#53fcd8] p-2 text-center font-bold w-32 h-10 hover:scale-105'>
+                            ${0.1} Buy
+                        </button>
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-2 md:grid-cols-3 text-center gap-2'>
+                    <div className='p-4 flex flex-col bg-rives-gray'>
+                        <span>Total Minted</span>
+                        <span className='mt-auto'>1.1k</span>
+                    </div>
+
+                    <div className='p-4 flex flex-col bg-rives-gray'>
+                        <span>Marketcap</span>
+                        <span className='mt-auto'>USD 5k</span>
+                    </div>
+
+                    <div className='p-4 flex flex-col bg-rives-gray'>
+                        <span>Total Owners</span>
+                        <span className='mt-auto'>100</span>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        Date: {new Date(tape._timestamp *1000).toLocaleString()}
+                    </div>
+
+                    <div>
+                        Rule: {contest.name}
+                    </div>
+
+                    <div>
+                        Score: {Number(tape.claimed_score._hex)}
+                    </div>
+
+                </div>
+
+                <ContestCard contest={{...contest, prize: ""}} cartridgeCover={tapeCartridge.cover} />
             </div>
         </main>
     )
