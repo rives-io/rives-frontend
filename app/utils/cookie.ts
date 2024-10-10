21e1468aaec2cead6746fe234051716eb3a769e4
Arthur <arthur@DESKTOP-C88PO09>
2024-10-10 12:03:24 -0300

    feat: domain consent cookie and expiration date

--- app/utils/cookie.ts
+++ app/utils/cookie.ts
@@ -6,5 +6,6 @@ import { cookies } from 'next/headers';
 
 
 export async function setCookie(name:string, value:string) {
-    cookies().set(name, value);
+    const month_ms = 60 * 60 * 24 * 30 * 1000;
+    cookies().set(name, value, {expires: Date.now() + month_ms, domain: ".rives.io"});
 }
